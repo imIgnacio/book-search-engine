4dd4b471474cc55c3c0a2cf686379c0a949230b9
Ignacio Diaz <nachodiaz99@gmail.com>
2022-01-28 15:25:27 +0400

    app working

---
+++ client/build/precache-manifest.0d2752e33b1df27941379f4299d521a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6287bacc23d8928aa60cf7fe37fc3eed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6d9dfe41ff652a04c59",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "4d6668da97ed3b6420c0",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "d6d9dfe41ff652a04c59",
+    "url": "/static/js/2.8b5a6783.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.8b5a6783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d6668da97ed3b6420c0",
+    "url": "/static/js/main.e300f4ec.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
